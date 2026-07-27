1474df6d98f6d679a69ab44b0c4a5d30384f3b8e
ArrudaG <gabrielarr1227@gmail.com>
2026-07-27 09:59:55 -0300

    .

--- src/models/notificacao_model.py
+++ src/models/notificacao_model.py
@@ -1,4 +1,4 @@
-﻿from sqlalchemy import String, Boolean, Integer, Float, DateTime
+﻿from sqlalchemy import String, Boolean, Integer, Float
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
