bba7a47a4514cd8b3c9fd6ed70f6dcdda220e483
Gabriel Arruda <108347889+ArrudaG@users.noreply.github.com>
2026-08-25 12:13:28 -0300

    Clean up main.py by removing blank lines
    Removed unnecessary blank lines in main.py.

--- main.py
+++ main.py
@@ -2,15 +2,12 @@ from src.services.acao_service import AcaoService
 from src.services.email_service import EmailService
 from src.services.notificacao_service import NotificacaoService
 
-
 def main():
     acao_service = AcaoService()
     email_service = EmailService()
     monitor = NotificacaoService(acao_service, email_service)
-
     acao_service.alterar_valor_ticker()
     monitor.retornar_pendentes()
-
 
 if __name__ == "__main__":
     main()
